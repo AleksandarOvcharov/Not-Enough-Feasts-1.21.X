e481152efeaa6f35d33509906f9026af3bb918b7
AleksandarOvcharov <klasatsiyabg@gmail.com>
2025-07-28 23:42:45 +0300

    Datagen updated

--- src/main/java/dev/valhal4o1331/notenoughfeasts/NotEnoughFeastsDataGenerator.java
+++ src/main/java/dev/valhal4o1331/notenoughfeasts/NotEnoughFeastsDataGenerator.java
@@ -1,11 +1,17 @@
 package dev.valhal4o1331.notenoughfeasts;
 
+import dev.valhal4o1331.notenoughfeasts.datagen.ModModelProvider;
+import dev.valhal4o1331.notenoughfeasts.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class NotEnoughFeastsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 
 	}
 }
---
+++ src/main/java/dev/valhal4o1331/notenoughfeasts/datagen/ModRecipeProvider.java
@@ -0,0 +1,33 @@
+package dev.valhal4o1331.notenoughfeasts.datagen;
+
+import dev.valhal4o1331.notenoughfeasts.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter exporter) {
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.APPLE_PIE, 1)
+                .input(Items.APPLE)
+                .input(Items.SUGAR)
+                .input(Items.EGG)
+                .criterion("has_apple", conditionsFromItem(Items.APPLE))
+                .criterion("has_sugar", conditionsFromItem(Items.SUGAR))
+                .criterion("has_egg", conditionsFromItem(Items.EGG))
+                .offerTo(exporter);
+
+
+    }
+}
