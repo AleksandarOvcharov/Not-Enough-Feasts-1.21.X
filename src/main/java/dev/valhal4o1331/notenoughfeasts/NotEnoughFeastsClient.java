cb6c92cb87085adf680e73cad6efbe2e1045b0c1
AleksandarOvcharov <klasatsiyabg@gmail.com>
2025-07-29 16:26:53 +0300

    Updated mod to 1.21.6

--- src/main/java/dev/valhal4o1331/notenoughfeasts/NotEnoughFeastsClient.java
+++ src/main/java/dev/valhal4o1331/notenoughfeasts/NotEnoughFeastsClient.java
@@ -2,13 +2,13 @@ package dev.valhal4o1331.notenoughfeasts;
 
 import dev.valhal4o1331.notenoughfeasts.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.BlockRenderLayer;
 
 public class NotEnoughFeastsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ONION_CROP, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ONION_BUSH, RenderLayer.getCutout());
+        BlockRenderLayerMap.putBlock(ModBlocks.ONION_CROP, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.ONION_BUSH, BlockRenderLayer.CUTOUT);
     }
 }
